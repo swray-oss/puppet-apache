52d873f0617984709e99005c3c584a76c0857c0c
Stephen Wray <stevedwray@gmail.com>
2021-05-20 13:16:07 +1200

    service management

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,10 +9,15 @@ class apache (
   String $install_ensure,
   String $config_ensure,
   String $config_path,
+  String $service_name,
+  String $service_enable,
+  Enum["running","stopped"] $service_ensure
 ) {
   contain apache::install
   contain apache::config
-
+  contain apache::service
+  
   Class['::apache::install']
   -> Class['::apache::config']
+  ~> Class['::apache::service']
 }
---
+++ manifests/service.pp
@@ -0,0 +1,11 @@
+# @summary
+#   Service control for apache
+class apache::service {
+  service { "${apache::service_name}":
+    alias      => $apache_service,
+    ensure     => $apache::service_ensure,
+    enable     => $apache::service_enable,
+    hasrestart => true,
+
+  }
+}
