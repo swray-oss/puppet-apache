f770aac1d39249de39b014fb183b89ebfa07ba80
Stephen Wray <stevedwray@gmail.com>
2021-05-24 11:32:58 +1200

    can I use custom fact in hiera?

--- lib/facter/webserver_type.rb
+++ lib/facter/webserver_type.rb
@@ -1,19 +1,21 @@
 Facter.add(:webserver, :type => :aggregate) do
-  osfamily = Facter.value(:osfamily)
-  value = {}
-  case osfamily
-  when 'Debian'
-    value['service'] = 'apache2'
-    value['package'] = 'apache2'
-    value['user']    = 'www-data'
-    value['group']   = 'www-data'
-    value
-  when 'RedHat'
-    value['service'] = 'httpd'
-    value['package'] = 'httpd'
-    value['user']    = 'apache'
-    value['group']   = 'apache'
-    value
+  chunk(:webserver_types) do
+    osfamily = Facter.value(:osfamily)
+    value = {}
+    case osfamily
+    when 'Debian'
+      value['service'] = 'apache2'
+      value['package'] = 'apache2'
+      value['user']    = 'www-data'
+      value['group']   = 'www-data'
+      value
+    when 'RedHat'
+      value['service'] = 'httpd'
+      value['package'] = 'httpd'
+      value['user']    = 'apache'
+      value['group']   = 'apache'
+      value
+    end
   end
 end
 
