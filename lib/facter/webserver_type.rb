4c0abac10af2ea54067ff709d3fb516598c08bed
Stephen Wray <stevedwray@gmail.com>
2021-05-24 12:06:52 +1200

    can I use custom fact in hiera?

--- lib/facter/webserver_type.rb
+++ lib/facter/webserver_type.rb
@@ -8,12 +8,16 @@ Facter.add(:webserver, :type => :aggregate) do
       value['package'] = 'apache2'
       value['user']    = 'www-data'
       value['group']   = 'www-data'
+      value['config_path'] = '/etc/apache2/apache2.conf'
+      value['vhosts_dir'] = '/etc/apache2/sites-available'
       value
     when 'RedHat'
       value['service'] = 'httpd'
       value['package'] = 'httpd'
       value['user']    = 'apache'
       value['group']   = 'apache'
+      value['config_path'] = '/etc/httpd/conf/httpd.conf'
+      value['vhosts_dir'] = '/etc/httpd/conf.d'
       value
     end
   end
